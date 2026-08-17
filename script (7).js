aa2ce39976412cde0cede55f7b2ab0f10167d898
hashircodingal <hashircodingal@gmail.com>
2026-08-17 15:09:06 +0500

    Add files via upload

---
+++ script (7).js
@@ -0,0 +1,16 @@
+<script>
+    var num1 = 10;
+    var num2 = 2;
+</script>
+var add = num1 + num2;
+var sub = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
+document.getElementById("add").innerHTML =
+"The added value is: " + add;
+document.getElementById("sub").innerHTML =
+"The subtracted value is: " + sub;
+document.getElementById("mul").innerHTML =
+"The multiplied value is: " + mul;
+document.getElementById("div").innerHTML =
+"The divided value is: " + div;
